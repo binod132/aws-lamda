d5773ab3fe3b98da0b0142c7ed4333be7e65f917
Binod <adhikaribinod132@gmail.com>
2025-08-28 13:23:21 +0545

    fixed

--- main.tf
+++ main.tf
@@ -5,16 +5,21 @@ provider "aws" {
 resource "aws_s3_bucket" "event_bucket" {
   bucket = var.lambda_s3_bucket
 
-  versioning {
-    enabled = true
-  }
-
   tags = {
     Name = "EventBucket"
   }
 }
 
-data "aws_s3_bucket_object" "lambda_zip" {
+# New separate resource for versioning (instead of deprecated block)
+resource "aws_s3_bucket_versioning" "event_bucket_versioning" {
+  bucket = aws_s3_bucket.event_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+data "aws_s3_object" "lambda_zip" {
   bucket = var.lambda_s3_bucket
   key    = var.lambda_s3_key
 }
@@ -22,11 +27,14 @@ data "aws_s3_bucket_object" "lambda_zip" {
 resource "aws_lambda_function" "processor" {
   function_name = "file_processor_lambda"
   role          = var.lambda_role_arn
-  handler       = "index.handler"   # Change if your Lambda handler differs
-  runtime       = "python3.9"       # Change runtime if needed
+  handler       = "index.handler"
+  runtime       = "python3.9"
 
   s3_bucket = var.lambda_s3_bucket
   s3_key    = var.lambda_s3_key
 
-  depends_on = [aws_s3_bucket.event_bucket]
+  depends_on = [
+    aws_s3_bucket.event_bucket,
+    aws_s3_bucket_versioning.event_bucket_versioning,
+  ]
 }
